e7ce276baaa664c46847c14badb8ffecdb337532
Julia <febstars@gmail.com>
2016-08-24 20:23:32 +0200

    function test

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var sayMyName = function (name) {
+	alert ('My name is '+name);
+}
+
+var car = {
+	make: 'Chrysler',
+	type: 'Cruiser',
+	color: 'green',
+	isTurnedOn: false,
+	numberOfWheels: 5,
+	seats: [
+		'seat1',
+		'seat2',
+		'seat3',
+		'seat4'
+	],
+	turnOn: function () {
+		this.isTurnedOn = true;
+	},
+	fly: function(){
+		alert('fly');
+	},
+	switchCar: function(isOn){
+		console.log('The car is turned on: '+isOn);
+		if (isOn == true){
+			this.isTurnedOn = true;
+		} else {
+			this.isTurnedOn = false;
+		}
+	}
+
+};
+
+console.log('good evening, dearest children');
